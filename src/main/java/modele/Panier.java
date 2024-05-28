a6f43e35386f6f8d9cea6e105f783b6b469a80ef
Ducceschi Nino <nino.ducceschi@etu.iut-tlse3.fr>
2024-05-28 15:52:13 +0200

    ajout de la doccumentation

--- src/main/java/modele/Panier.java
+++ src/main/java/modele/Panier.java
@@ -11,6 +11,12 @@ public class Panier {
         this.articles = new LinkedList<ArticleSelectionne>();
     }
 
+    /**
+     * Ajoute un article au panier
+     * si l'article est déjà présent, la quantité est augmentée
+     * @param article Artible à ajouter
+     * @param qte Quantité à ajouter
+     */
     public void addArticle(Article article, int qte) {
         if (this.contient(article.getFromage())) {
             this.getArticle(article.getFromage()).ajouter(qte);
@@ -18,10 +24,26 @@ public class Panier {
         this.articles.add(new ArticleSelectionne(article, qte));
     }
 
+    /**
+     * Supprime un article du panier
+     * @param article Article à supprimer
+     */
     public void removeArticle(Article article) {
-        this.articles.remove(article);
+        this.articles.remove(this.getArticle(article.getFromage()));
     }
 
+    /**
+     * Supprime un article du panier
+     * @param articleSelectionne Article à supprimer
+     */
+    public void removeArticle(ArticleSelectionne articleSelectionne) {
+        this.articles.remove(articleSelectionne);
+    }
+
+    /**
+     * Retourne une représentation textuelle du panier
+     * @return Représentation textuelle du panier
+     */
     public String toString() {
         StringBuffer enForme = new StringBuffer();
         for (ArticleSelectionne article : this.articles) {
@@ -30,6 +52,10 @@ public class Panier {
         return enForme.toString();
     }
 
+    /**
+     * Retourne le prix total du panier
+     * @return Prix total du panier
+     */
     public double prixTotal() {
         double prixTotal = 0;
         for (ArticleSelectionne article : this.articles) {
@@ -38,22 +64,43 @@ public class Panier {
         return prixTotal;
     }
 
+    /**
+     * Retourne le nombre d'articles dans le panier
+     * @return Nombre d'articles dans le panier
+     */
     public int nombreArticles() {
         return this.articles.size();
     }
 
+    /**
+     * Retourne la liste des articles du panier
+     * @return Liste des articles du panier
+     */
     public List<ArticleSelectionne> getArticles() {
         return this.articles;
     }
 
+    /**
+     * Retourne l'article à l'index donné
+     * @param index Index de l'article
+     * @return Article à l'index donné
+     */
     public ArticleSelectionne getArticle(int index) {
         return this.articles.get(index);
     }
 
+    /**
+     * Vide le panier
+     */
     public void vider() {
         this.articles.clear();
     }
 
+    /**
+     * Retourne vrai si le panier contient l'article donné
+     * @param fromage Fromage à chercher
+     * @return Vrai si le panier contient l'article donné
+     */
     public boolean contient(Fromage fromage) {
         for (ArticleSelectionne article : this.articles) {
             if (article.getFromage().equals(fromage)) {
@@ -63,6 +110,11 @@ public class Panier {
         return false;
     }
 
+    /**
+     * Retourne l'article correspondant au fromage donné
+     * @param fromage Fromage à chercher
+     * @return Article correspondant au fromage donné
+     */
     public ArticleSelectionne getArticle(Fromage fromage) {
         for (ArticleSelectionne article : this.articles) {
             if (article.getFromage().equals(fromage)) {
