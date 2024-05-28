624cb3f8c83f86142daa50b888d7d2d602fc2c45
Poux- - Bories Yoan <yoan.poux-bories@etu.iut-tlse3.fr>
2024-05-28 15:39:35 +0200

    Début recherche

---
+++ src/main/java/modele/Facture.java
@@ -0,0 +1,12 @@
+package modele;
+
+public class Facture {
+    private String nom_client;
+    private String prenom_client;
+    private String adresse_livraison_client;
+    private String numero_client;
+
+    private Panier panier;
+
+    public Facture() {}
+}
