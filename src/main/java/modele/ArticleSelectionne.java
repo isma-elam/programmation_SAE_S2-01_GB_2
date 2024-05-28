a05f040d947e2419e3ae3a9a1c7dbb87a9bf35e6
Ducceschi Nino <nino.ducceschi@etu.iut-tlse3.fr>
2024-05-28 16:46:30 +0200

    comments

--- src/main/java/modele/ArticleSelectionne.java
+++ src/main/java/modele/ArticleSelectionne.java
@@ -1,7 +1,7 @@
 package modele;
 
 public class ArticleSelectionne {
-    private Article article;
+    private final Article article;
 
     private int quantite;
 
@@ -10,22 +10,40 @@ public class ArticleSelectionne {
         this.quantite = quantite;
     }
 
+    /**
+     * Retourne le montant total de l'article selectionné
+     * @return Montant total de l'article selectionné (prix TTC * quantité)
+     */
     public double total() {
         return this.article.getPrixTTC() * this.quantite;
     }
 
+    /**
+     * Retourne le fromage de l'article selectionné
+     * @return Fromage de l'article selectionné
+     */
     public Object getFromage() {
         return this.article.getFromage();
     }
 
+
+    @Override
     public String toString() {
         return this.article.toString() + ", Quantite : " + this.quantite;
     }
 
+    /**
+     * Retourne la quantité de l'article selectionné
+     * @return Quantité de l'article selectionné
+     */
     public int getQuantite() {
         return this.quantite;
     }
 
+    /**
+     * Modifie la quantité de l'article selectionné
+     * @param quantite Nouvelle quantité
+     */
     public void setQuantite(int quantite) {
         if (article.getQuantiteEnStock() < quantite) {
             throw new IllegalArgumentException("Quantite en stock insuffisante");
@@ -33,6 +51,10 @@ public class ArticleSelectionne {
         this.quantite = quantite;
     }
 
+    /**
+     * Ajoute une quantité à l'article selectionné si la quantité en stock est suffisante
+     * @param quantite Quantité à ajouter
+     */
     public void ajouter(int quantite) {
         if (article.getQuantiteEnStock() < this.quantite + quantite) {
             throw new IllegalArgumentException("Quantite en stock insuffisante");
