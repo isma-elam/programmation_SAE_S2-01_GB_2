7e31587ed472a4fa41c5bf2f04adbc46ccd28d94
Poux- - Bories Yoan <yoan.poux-bories@etu.iut-tlse3.fr>
2024-05-28 17:58:21 +0200

    Début recherche

--- src/main/java/modele/Recherche.java
+++ src/main/java/modele/Recherche.java
@@ -6,10 +6,12 @@ import java.util.List;
 public class Recherche {
     private String key;
     private List<Fromage> fromages;
+    private List<Filtres> filtres;
 
     public Recherche(String key, List<Fromage> fromages, List<Filtres>) {
         this.key = key;
         this.fromages = fromages;
+        this.filtres = filtres;
     }
 
     public List<Fromage> getResultat (){
@@ -18,5 +20,7 @@ public class Recherche {
         for (Fromage fromage : fromages) {
             if ()
         }
+
+        return resultat;
     }
 }
