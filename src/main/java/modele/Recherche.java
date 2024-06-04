49735eeabbf34c3ba93add67d9e8ecd98cb72fc7
Ducceschi Nino <nino.ducceschi@etu.iut-tlse3.fr>
2024-06-04 14:40:02 +0200

    temporaire, le temps de build

--- src/main/java/modele/Recherche.java
+++ src/main/java/modele/Recherche.java
@@ -8,19 +8,20 @@ public class Recherche {
     private List<Fromage> fromages;
     private List<Filtres> filtres;
 
-    public Recherche(String key, List<Fromage> fromages, List<Filtres>) {
+    public Recherche(String key, List<Fromage> fromages, List<Filtres> filtres) {
         this.key = key;
         this.fromages = fromages;
         this.filtres = filtres;
     }
 
     public List<Fromage> getResultat (){
-        List<Fromage> resultat = new ArrayList<Fromage>();
-
-        for (Fromage fromage : fromages) {
-            if ()
-        }
-
-        return resultat;
+//        List<Fromage> resultat = new ArrayList<Fromage>();
+//
+//        for (Fromage fromage : fromages) {
+//            if ()
+//        }
+//
+//        return resultat;
+        return null;
     }
 }
